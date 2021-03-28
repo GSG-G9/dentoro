e174c3ffe09549025688d4d83a20e76ba6914e06
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-03-28 08:06:38 +0300

    addinf the connection file and creating the data base localy , adding the .env file localy Relates #7

---
+++ server/database/config/connection.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const { Pool } = require('pg');
+
+let dbUrl = '';
+const {
+  env: { NODE_ENV, TEST_DB_URL, DEV_DB_URL, DATABASE_URL },
+} = process;
+
+switch (NODE_ENV) {
+  case 'production':
+    dbUrl = DATABASE_URL;
+    break;
+
+  case 'development':
+    dbUrl = DEV_DB_URL;
+    break;
+
+  case 'test':
+    dbUrl = TEST_DB_URL;
+    break;
+
+  default:
+    throw new Error('There is no Database url !!!');
+}
+
+const option = {
+  connectionString: dbUrl,
+  ssl: !!process.env.DATABASE_URL,
+};
+
+module.exports = new Pool(option);
