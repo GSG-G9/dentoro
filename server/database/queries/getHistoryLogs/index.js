2c66b53fed4dc16b3e16c2122c3bdbef975de497
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-03-29 20:12:51 +0300

    adding testing to the route and removing boom Relates #12

--- server/database/queries/getHistoryLogs/index.js
+++ server/database/queries/getHistoryLogs/index.js
@@ -1,4 +1,4 @@
-const connection = require('../config/connection');
+const connection = require('../../config/connection');
 
 const getHistoryLogs = ({ patientId }) => {
   const sql = {
--- server/database/queries/getPatientProfileData/index.js
+++ server/database/queries/getPatientProfileData/index.js
@@ -1,4 +1,4 @@
-const connection = require('../config/connection');
+const connection = require('../../config/connection');
 
 const getPatientProfileData = ({ patientId }) => {
   const sql = {
