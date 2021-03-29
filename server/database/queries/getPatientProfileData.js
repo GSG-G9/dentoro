03ce56701b655f66c678f8eb43f5790c39b89602
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-03-29 13:05:04 +0300

    creating a query to fetch all profile data Relates #12

---
+++ server/database/queries/getPatientProfileData.js
@@ -0,0 +1,11 @@
+const connection = require('../config/connection');
+
+const getPatientProfileData = ({ patientId }) => {
+  const sql = {
+    text: 'SELECT * FROM patients where id = $1 ;',
+    values: [patientId],
+  };
+  return connection.query(sql);
+};
+
+module.exports = getPatientProfileData;
