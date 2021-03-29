11541e2020982722b7cb18920f8c5bf92297e7de
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-03-29 20:33:04 +0300

    removing un needed commints

--- server/controllers/appointments/getAvailableAppointments/index.js
+++ server/controllers/appointments/getAvailableAppointments/index.js
@@ -31,7 +31,6 @@ const getAllProfileData = async (req, res, next) => {
     const availableTimeSet = new Set(availableTime);
 
     const { rows: unavailable } = await getUnavailableTimes({ date });
-    console.log(unavailable);
 
     unavailable.forEach((element) => {
       const { appointment_time: appointmentTime } = element;
