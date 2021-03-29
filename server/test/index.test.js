2454ce61d02752bc392aed9b0639848af4e7b25c
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-03-29 10:03:26 +0300

    remove console.log from index.test.js

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -46,7 +46,6 @@ describe('Server Tests', () => {
         },
       ];
       const { rows } = await getAppointmentsByDate('2020-12-2');
-      console.log(rows);
       return expect(expected).toEqual(rows);
     });
   });
