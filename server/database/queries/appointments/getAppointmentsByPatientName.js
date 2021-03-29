caf0679409f111f5fe33a8634458f4cec305c06d
Alaa Alser <alaalsser@gmail.com>
2021-03-30 01:21:33 +0300

    edit qeury Relates #13

--- server/database/queries/appointments/getAppointmentsByPatientName.js
+++ server/database/queries/appointments/getAppointmentsByPatientName.js
@@ -1,9 +1,9 @@
 const connection = require('../../config/connection');
 
-const getAppointmentsByPatientName = (firstName, lastName, phone) => {
+const getAppointmentsByPatientName = ({ firstName, lastName, phone }) => {
   const sql = {
     text:
-      'SELECT appointments.id,appointments.appointment_date,appointments.appointment_time,patients.firstname,patients.lastname FROM appointments INNER JOIN patients ON patients.id = appointments.patient_id where appointments.is_done =false AND patients.firstname=$1 AND patients.lastname=$2 OR (patients.phone=$3 AND appointments.is_done =false);',
+      'SELECT appointments.id,appointments.appointment_date,appointments.appointment_time,patients.firstname,patients.lastname FROM appointments INNER JOIN patients ON patients.id = appointments.patient_id where appointments.is_done =false AND patients.firstname = $1 AND patients.lastname = $2 OR (patients.phone= $3 AND appointments.is_done =false) ;',
     values: [firstName, lastName, phone],
   };
   return connection.query(sql);
