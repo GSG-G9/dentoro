b0243b9f76ccd6dce2d526c95c5ad0a536434070
Alaa Alser <alaalsser@gmail.com>
2021-03-30 17:15:41 +0300

    create delete query Relates #19

---
+++ server/database/queries/deleteAppointmentsQueries/index.js
@@ -0,0 +1,12 @@
+const connection = require('../../config/connection');
+
+const deleteAppointmentsQueries = (patientId, appointmentId) => {
+  const sql = {
+    text:
+      'DELETE FROM appointments WHERE patient_id = (SELECT id FROM patients WHERE id = $1) AND id = $2 RETURNING * ;',
+    values: [patientId, appointmentId],
+  };
+  return connection.query(sql);
+};
+
+module.exports = { deleteAppointmentsQueries };
