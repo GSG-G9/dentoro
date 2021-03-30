6f647df0136b78e0ded6d334b86d71ba0f453934
Alaa Alser <alaalsser@gmail.com>
2021-03-30 17:52:44 +0300

    fix export query Relates #19

--- server/database/queries/deleteAppointmentsQueries/index.js
+++ server/database/queries/deleteAppointmentsQueries/index.js
@@ -1,6 +1,6 @@
 const connection = require('../../config/connection');
 
-const deleteAppointmentsQueries = (patientId, appointmentId) => {
+const deleteAppointmentsQueries = ({ patientId, appointmentId }) => {
   const sql = {
     text:
       'DELETE FROM appointments WHERE patient_id = (SELECT id FROM patients WHERE id = $1) AND id = $2 RETURNING * ;',
@@ -9,4 +9,4 @@ const deleteAppointmentsQueries = (patientId, appointmentId) => {
   return connection.query(sql);
 };
 
-module.exports = { deleteAppointmentsQueries };
+module.exports = deleteAppointmentsQueries;
