ac43488596646eaff9333d8d7437abb0c962d235
Alaa Alser <alaalsser@gmail.com>
2021-03-30 18:23:14 +0300

    fix query values

--- server/database/queries/deleteAppointmentsQueries/index.js
+++ server/database/queries/deleteAppointmentsQueries/index.js
@@ -1,6 +1,6 @@
 const connection = require('../../config/connection');
 
-const deleteAppointmentsQueries = ({ patientId, appointmentId }) => {
+const deleteAppointmentsQueries = (patientId, appointmentId) => {
   const sql = {
     text:
       'DELETE FROM appointments WHERE patient_id = (SELECT id FROM patients WHERE id = $1) AND id = $2 RETURNING * ;',
