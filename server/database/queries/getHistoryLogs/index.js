838250c79438a5b89e02c3f217d550d662682979
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-03-31 18:39:08 +0300

    fixing the database by adding a data to the history and fexing the get all data route

--- server/database/queries/getHistoryLogs/index.js
+++ server/database/queries/getHistoryLogs/index.js
@@ -3,7 +3,7 @@ const connection = require('../../config/connection');
 const getHistoryLogs = ({ patientId }) => {
   const sql = {
     text:
-      'SELECT history.price, history.payment, history.description, appointments.appointment_date FROM history INNER JOIN appointments ON appointments.id = history.appointment_id AND appointments.patient_id = $1 AND is_done = true;',
+      'SELECT price, payment, description, log_date FROM history where patient_id = $1 ',
     values: [patientId],
   };
   return connection.query(sql);
