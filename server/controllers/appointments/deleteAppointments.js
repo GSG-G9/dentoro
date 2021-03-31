f699668b3ae17d3eb2e14ca2effa5871988cb0be
Alaa Alser <alaalsser@gmail.com>
2021-03-31 13:36:48 +0300

    add oppointmentId validation Relates #19

--- server/controllers/appointments/deleteAppointments.js
+++ server/controllers/appointments/deleteAppointments.js
@@ -1,16 +1,23 @@
 const { deleteAppointmentsQueries } = require('../../database/queries');
+const { appointmentIdValidation, boomify } = require('../../utils');
 
 const deleteAppointments = async (req, res, next) => {
   try {
-    const { appointmentId } = req.params;
+    const { appointmentId } = await appointmentIdValidation.validate(
+      req.params,
+    );
 
     await deleteAppointmentsQueries(appointmentId);
     return res.json({
       statusCode: 200,
       message: 'appointment deleted successfully',
     });
-  } catch (err) {
-    return next(err);
+  } catch (error) {
+    return next(
+      error.name === 'ValidationError'
+        ? boomify(400, 'Validation Error', error.errors)
+        : error,
+    );
   }
 };
 
