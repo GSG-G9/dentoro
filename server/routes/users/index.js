9525d69899937bbe2a679a335e6280b4d60b9bc1
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-04-04 11:47:33 +0300

    creating the routes and connecting it to the router Relates #18

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 require('../../controllers');
+const { addUser } = require('../../controllers');
 
 router.post('/login');
 router.get('/logout');
-router.post('/signup');
+router.post('/signup', addUser);
 
 module.exports = router;
