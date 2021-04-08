7676b4d8c55b5020f2e1304bb7bf49d888972dbd
Alaa Alser <alaalsser@gmail.com>
2021-04-08 19:32:30 +0300

    edit style

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
