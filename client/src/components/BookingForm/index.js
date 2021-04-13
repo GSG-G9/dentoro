ec4bf2cfdcf640423d0f67f6359f96c5a67e3c0a
Alaa Alser <alaalsser@gmail.com>
2021-04-14 00:25:02 +0300

    Handling form messages Relates #59

--- client/src/components/BookingForm/index.js
+++ client/src/components/BookingForm/index.js
@@ -1,33 +1,9 @@
-import React from 'react';
-import { Form, Input, Button, Row, Typography } from 'antd';
+import React, { useState } from 'react';
+import { Form, Input, Button, Row, Typography, Result, Alert } from 'antd';
 import axios from 'axios';
 import './style.css';
 
 const { Title } = Typography;
-
-const onFinish = async ({
-  firstName,
-  lastName,
-  email,
-  birthday,
-  phone,
-  diseases,
-  appointmentDate,
-  appointmentTime,
-  complaints,
-}) => {
-  await axios.post('/api/v1/appointments', {
-    firstName,
-    lastName,
-    email,
-    birthday,
-    phone,
-    diseases,
-    appointmentDate,
-    appointmentTime,
-    complaints,
-  });
-};
 
 const layout = {
   labelCol: { span: 8 },
@@ -37,86 +13,126 @@ const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
 };
 
-const BookingForm = () => (
-  <div className="booking">
-    <Form
-      style={{ marginLeft: '20rem' }}
-      {...layout}
-      onFinish={onFinish}
-      name="basic"
-    >
-      <Title color="#fff"> Book an appointment</Title>
-      <Row>
-        <Form.Item
-          name="firstName"
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-          <Input
-            placeholder="First Name"
-            style={{ width: '12rem', marginRight: '1rem' }}
+const BookingForm = () => {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState();
+
+  const onFinish = async ({
+    firstName,
+    lastName,
+    email,
+    birthday,
+    phone,
+    diseases,
+    appointmentDate,
+    appointmentTime,
+    complaints,
+  }) => {
+    try {
+      setSuccess(false);
+      await axios.post('/api/v1/appointments', {
+        firstName,
+        lastName,
+        email,
+        birthday,
+        phone,
+        diseases,
+        appointmentDate,
+        appointmentTime,
+        complaints,
+      });
+      setSuccess(true);
+    } catch (err) {
+      let e;
+      if (err.response.data.error === 'Unavailable Time') {
+        e = 'please choose another appointment time';
+      } else if (err.response.data.error === 'Validation Error') {
+        e = err.response.data.message;
+      } else if (err.response.data.error === 'RangeError') {
+        e = 'Input valid date in form YYYY-MM-DD or time hh:mm:ss ';
+      } else {
+        e = 'Sever error, please try again later';
+      }
+      setError(e);
+    }
+  };
+
+  return (
+    <div className="booking">
+      <Form {...layout} onFinish={onFinish} name="basic">
+        <Title color="#fff"> Book an appointment</Title>
+        {error && (
+          <Alert
+            className="err"
+            id="alert"
+            message={error}
+            type="error"
+            showIcon
           />
+        )}
+        <Row>
+          <Form.Item
+            name="firstName"
+            rules={[{ required: true, message: 'Please input first Name!' }]}
+          >
+            <Input className="first-name" placeholder="First Name" />
+          </Form.Item>
+          <Form.Item
+            name="lastName"
+            rules={[{ required: true, message: 'Please input last name!' }]}
+          >
+            <Input className="last-name" placeholder="Last Name" />
+          </Form.Item>
+        </Row>
+        <Form.Item name="email">
+          <Input placeholder="Email" />
+        </Form.Item>
+        <Form.Item name="birthday">
+          <Input placeholder="Birthday" />
         </Form.Item>
         <Form.Item
-          name="lastName"
-          rules={[{ required: true, message: 'Please input your username!' }]}
+          name="phone"
+          rules={[
+            { required: true, message: 'Please input your phone number!' },
+          ]}
         >
-          <Input placeholder="Last Name" style={{ width: '12rem' }} />
+          <Input placeholder="Phone" />
         </Form.Item>
-      </Row>
 
-      <Form.Item
-        name="email"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input placeholder="Email" />
-      </Form.Item>
-      <Form.Item
-        name="birthday"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input placeholder="Birthday" />
-      </Form.Item>
-      <Form.Item
-        name="phone"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input placeholder="Phone" />
-      </Form.Item>
+        <Form.Item
+          name="appointmentDate"
+          rules={[
+            { required: true, message: 'Please input your appointment date!' },
+          ]}
+        >
+          <Input placeholder="Appointment date" />
+        </Form.Item>
+        <Form.Item
+          name="appointmentTime"
+          rules={[
+            { required: true, message: 'Please input appointment time!' },
+          ]}
+        >
+          <Input placeholder="Appointment time" />
+        </Form.Item>
+        <Form.Item name="diseases">
+          <Input placeholder="Diseases" className="input-height" />
+        </Form.Item>
+        <Form.Item name="complaints">
+          <Input placeholder="Complaints" className="input-height" />
+        </Form.Item>
 
-      <Form.Item
-        name="appointmentDate"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input placeholder="Appointment date" />
-      </Form.Item>
-      <Form.Item
-        name="appointmentTime"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input placeholder="Appointment time" />
-      </Form.Item>
-      <Form.Item
-        name="diseases"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input placeholder="Diseases" style={{ height: '5rem' }} />
-      </Form.Item>
-      <Form.Item
-        name="complaints"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input placeholder="Complaints" style={{ height: '5rem' }} />
-      </Form.Item>
+        <Form.Item {...tailLayout} name="remember" valuePropName="checked" />
 
-      <Form.Item {...tailLayout} name="remember" valuePropName="checked" />
-
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Confirm
-        </Button>
-      </Form.Item>
-    </Form>
-  </div>
-);
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Confirm
+          </Button>
+        </Form.Item>
+      </Form>
+      {success && <Result status="success" title="Booked successfully" />}
+    </div>
+  );
+};
 
 export default BookingForm;
