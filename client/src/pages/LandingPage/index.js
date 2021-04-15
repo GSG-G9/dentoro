d352864cc6e61deda21c6af58c31d640a059c59b
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-04-15 18:16:52 +0300

    create about us page

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import NavBar from '../../components/NavBar';
 import BookingForm from '../../components/BookingForm';
 import Map from '../home/Map';
+import AboutUs from '../home/AboutUs';
+import './style.css';
 
 const LandingPage = () => (
-  <div>
-    <NavBar />
-    <BookingForm />
-    <Map />;
+  <div className="landing-page-container">
+    <div className="landing-page-content">
+      <NavBar />
+      <AboutUs />
+      <BookingForm />
+      <Map />;
+    </div>
   </div>
 );
 
