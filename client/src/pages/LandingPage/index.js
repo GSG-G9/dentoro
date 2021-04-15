33ab89d3f9277c03c43ff9e5cf860f4419f4f1d6
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-04-15 23:12:22 +0300

    adding the header commponent t the landing page

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -3,12 +3,14 @@ import NavBar from '../../components/NavBar';
 import BookingForm from '../../components/BookingForm';
 import Map from '../home/Map';
 import AboutUs from '../home/AboutUs';
+import Header from '../../components/header';
 import './style.css';
 
 const LandingPage = () => (
   <div className="landing-page-container">
     <div className="landing-page-content">
       <NavBar />
+      <Header />
       <AboutUs />
       <BookingForm />
       <Map />;
