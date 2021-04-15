ef8c5709705f132481add549fa2e87c7aa7ecdb0
Alaa Alser <alaalsser@gmail.com>
2021-04-15 21:55:05 +0300

    footer Relates #50

--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import NavBar from '../../components/NavBar';
 import BookingForm from '../../components/BookingForm';
 import Map from '../home/Map';
+import Footer from '../../components/Footer';
 
 const LandingPage = () => (
   <div>
     <NavBar />
     <BookingForm />
-    <Map />;
+    <Map />
+    <Footer />
   </div>
 );
 
