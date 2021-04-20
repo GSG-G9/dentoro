001a98061bf50b61fc5432d71305621c02dac69e
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-04-20 03:35:16 +0300

    fix map style - add modal button instead of list in mobile - tablet screen size - fix form and add and image to fill the gab

--- client/src/components/BookingForm/index.js
+++ client/src/components/BookingForm/index.js
@@ -3,7 +3,6 @@ import {
   Form,
   Input,
   Button,
-  Row,
   Typography,
   DatePicker,
   TimePicker,
@@ -12,7 +11,9 @@ import {
 } from 'antd';
 import axios from 'axios';
 import moment from 'moment';
+import Image from '../common/Image';
 import './style.css';
+import bookingFormImage from '../../assets/images/undraw_Booking_re_gw4j.svg';
 
 const { Title } = Typography;
 
@@ -78,95 +79,142 @@ const BookingForm = () => {
           </Button>
         </div>
       ) : (
-        <Form onFinish={onFinish} name="basic">
-          <Title color="#fff"> Book an appointment</Title>
-          {error && (
-            <Alert
-              className="err"
-              id="alert"
-              message={error}
-              type="error"
-              showIcon
-            />
-          )}
-          <Row>
+        <div className="booking-form-form-container">
+          <Form onFinish={onFinish} name="basic" className="booking-form-form">
+            <Title level={3} className="booking-form-title">
+              Book an appointment
+            </Title>
+            {error && (
+              <Alert
+                className="err"
+                id="alert"
+                message={error}
+                type="error"
+                showIcon
+              />
+            )}
+            <div className="booking-form-row">
+              <Form.Item
+                className="booking-form-item"
+                name="firstName"
+                rules={[
+                  { required: true, message: 'Please input first Name!' },
+                ]}
+              >
+                <Input placeholder="First Name" />
+              </Form.Item>
+              <Form.Item
+                className="booking-form-item"
+                name="lastName"
+                rules={[{ required: true, message: 'Please input last name!' }]}
+              >
+                <Input placeholder="Last Name" />
+              </Form.Item>
+            </div>
+            <div className="booking-form-row">
+              <Form.Item
+                className="booking-form-item"
+                name="email"
+                rules={[{ type: 'email' }]}
+              >
+                <Input placeholder="Email" />
+              </Form.Item>
+              <Form.Item
+                className="booking-form-item"
+                name="birthday"
+                placeholder="Birthday"
+              >
+                <DatePicker placeholder="Birthday" />
+              </Form.Item>
+            </div>
+
             <Form.Item
-              name="firstName"
-              rules={[{ required: true, message: 'Please input first Name!' }]}
+              className="booking-form-item-phone"
+              name="phone"
+              rules={[
+                { required: true, message: 'Please input your phone number!' },
+                {
+                  pattern: /^[0-9-]*[0-9].{9,}$/,
+                  message: 'Invalid phone number',
+                },
+              ]}
             >
-              <Input className="first-name" placeholder="First Name" />
+              <Input placeholder="Phone" />
             </Form.Item>
-            <Form.Item
-              name="lastName"
-              rules={[{ required: true, message: 'Please input last name!' }]}
-            >
-              <Input className="last-name" placeholder="Last Name" />
+            <div className="booking-form-row">
+              <Form.Item
+                className="booking-form-item"
+                name="appointmentDate"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your appointment date!',
+                  },
+                ]}
+              >
+                <DatePicker
+                  disabledDate={disabledDate}
+                  placeholder="Appointment date"
+                />
+              </Form.Item>
+              <Form.Item
+                className="booking-form-item"
+                name="appointmentTime"
+                rules={[
+                  { required: true, message: 'Please input appointment time!' },
+                ]}
+              >
+                <TimePicker
+                  placeholder="Appointment time"
+                  format="HH"
+                  disabledHours={() => [
+                    0,
+                    1,
+                    2,
+                    3,
+                    4,
+                    5,
+                    6,
+                    7,
+                    19,
+                    20,
+                    21,
+                    22,
+                    23,
+                  ]}
+                  hideDisabledOptions
+                  showNow={false}
+                />
+              </Form.Item>
+            </div>
+            <div className="booking-form-row">
+              <Form.Item name="diseases" className="booking-form-item">
+                <Input.TextArea
+                  placeholder="Do you have any chronic diseases ?"
+                  className="input-height"
+                />
+              </Form.Item>
+              <Form.Item className="booking-form-item" name="complaints">
+                <Input.TextArea
+                  placeholder="What are you suffering from ?"
+                  className="input-height"
+                />
+              </Form.Item>
+            </div>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" loading={loading}>
+                Confirm
+              </Button>
             </Form.Item>
-          </Row>
-          <Form.Item name="email" rules={[{ type: 'email' }]}>
-            <Input placeholder="Email" />
-          </Form.Item>
-          <Form.Item name="birthday" placeholder="Birthday">
-            <DatePicker placeholder="Birthday" />
-          </Form.Item>
-          <Form.Item
-            name="phone"
-            rules={[
-              { required: true, message: 'Please input your phone number!' },
-              {
-                pattern: /^[0-9-]*[0-9].{9,}$/,
-                message: 'Invalid phone number',
-              },
-            ]}
-          >
-            <Input placeholder="Phone" />
-          </Form.Item>
-          <Form.Item
-            name="appointmentDate"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your appointment date!',
-              },
-            ]}
-          >
-            <DatePicker
-              disabledDate={disabledDate}
-              placeholder="Appointment date"
+          </Form>
+          <div className="booking-form-image-container">
+            <Image
+              src={bookingFormImage}
+              alt=""
+              className="booking-form-image"
             />
-          </Form.Item>
-          <Form.Item
-            name="appointmentTime"
-            rules={[
-              { required: true, message: 'Please input appointment time!' },
-            ]}
-          >
-            <TimePicker
-              placeholder="Appointment time"
-              format="HH"
-              disabledHours={() => [0, 1, 2, 3, 4, 5, 6, 7, 19, 20, 21, 22, 23]}
-              hideDisabledOptions
-              showNow={false}
-            />
-          </Form.Item>
-          <Form.Item name="diseases">
-            <Input.TextArea
-              placeholder="Do you have any chronic diseases ?"
-              className="input-height"
-            />
-          </Form.Item>
-          <Form.Item name="complaints">
-            <Input.TextArea
-              placeholder="What are you suffering from ?"
-              className="input-height"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" loading={loading}>
-              Confirm
-            </Button>
-          </Form.Item>
-        </Form>
+          </div>
+        </div>
       )}
     </div>
   );
