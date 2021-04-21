fa6a03c09002da19ca66e4a2237b8a49462c338e
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-04-21 04:04:57 +0300

    add % to patientSearchValidation schema relates #118

--- server/utils/validation/patientSearchValidation/index.js
+++ server/utils/validation/patientSearchValidation/index.js
@@ -2,11 +2,11 @@ const { object, string } = require('yup');
 
 const patientSearchValidation = object({
   firstName: string()
-    .matches(/^[a-zA-Z]+$/)
+    .matches(/^[a-zA-Z%]+$/)
     .min(3)
     .max(50),
   lastName: string()
-    .matches(/^[a-zA-Z]+$/)
+    .matches(/^[a-zA-Z%]+$/)
     .min(3)
     .max(50),
   phone: string().matches(/^\d+$/).min(9).max(14),
